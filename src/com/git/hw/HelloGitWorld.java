549d73e787fb666ae30a99b0402ce164e35f9fd3
Michael Goldverg <michael.goldverg@gmail.com>
2015-02-07 23:14:02 -0500

    Added another message

--- src/com/git/hw/HelloGitWorld.java
+++ src/com/git/hw/HelloGitWorld.java
@@ -4,8 +4,9 @@ public class HelloGitWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("Hello Git World!");
+		System.out.println("Goodbye Subversion World!");
 
 	}
 
